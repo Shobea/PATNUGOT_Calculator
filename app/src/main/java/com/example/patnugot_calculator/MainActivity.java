2a4124e7a8cece0d20dafd4cba24dda462f724f6
Bea Patnugot <97040197+Shobea@users.noreply.github.com>
2022-03-06 15:11:27 +0800

    Added comments

--- app/src/main/java/com/example/patnugot_calculator/MainActivity.java
+++ app/src/main/java/com/example/patnugot_calculator/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.patnugot_calculator;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -8,28 +10,23 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
     String oldNum="";
     String op = "+";
-    boolean newOp = true;
+    boolean NewOp = true;
     EditText input;
     Button clear;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        clear= findViewById(R.id.clearbtn);
         input = findViewById(R.id.input);
-        clear=findViewById(R.id.clearbtn);
-
-
     }
 
-
-
     public void numberSetup(View view) {
-        if(newOp)
+        if(NewOp) //If a button is clicked, input will be cleared. (Default zero is removed)
             input.setText("");
-        newOp = false;
+        NewOp = false;
         String number = input.getText().toString();
-        switch (view.getId()){
+        switch (view.getId()){         //When buttons are clicked, the numbers will concatenate
             case R.id.onebtn:
                 number += "1";
                 break;
@@ -63,57 +60,56 @@ public class MainActivity extends AppCompatActivity {
             case R.id.decimal:
                 number += ".";
                 break;
-
         }
         input.setText(number);
     }
 
-    public void operatorSetup(View view) {
-        newOp = true;
+    public void operatorSetup(View view) { //when button is clicked, op is assigned to an operator. Once button is clicked, input will be stored to oldNum
+        NewOp = true;
         oldNum = input.getText().toString();
         switch (view.getId()){
-                case R.id.dividebtn:
-                op = "/";
-                break;
-                case R.id.multiplybtn:
-                op = "*";
-                break;
-                case R.id.plusbtn:
+            case R.id.plusbtn:
                 op = "+";
-                break;
-                case R.id.minusbtn:
+            break;
+
+            case R.id.minusbtn:
                 op = "-";
+                break;
+
+            case R.id.multiplybtn:
+                op = "*";
+            break;
+
+            case R.id.dividebtn:
+                op = "/";
                 break;
         }
     }
 
-    public void solution(View view) {
+    @SuppressLint("SetTextI18n")
+    public void solution(View view) { //If = button is clicked, new input will be stored to newNumber
         String newNumber = input.getText().toString();
         double result = 0.0;
-        switch (op){
-            case "addition":
+        switch (op){ //Depending on the button clicked in operatorSetup, the numbers will be calculated according to the operator
+            case "+":
                 result = Double.parseDouble(oldNum)+ Double.parseDouble(newNumber);
                 break;
-            case "subtraction":
+            case "-":
                 result = Double.parseDouble(oldNum)- Double.parseDouble(newNumber);
                 break;
-            case "multiplication":
+            case "*":
                 result = Double.parseDouble(oldNum)* Double.parseDouble(newNumber);
                 break;
-            case "division":
+            case "/":
                 result = Double.parseDouble(oldNum)/ Double.parseDouble(newNumber);
                 break;
         }
         input.setText(result+"");
 
-        clear.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (input.getText().length() > 0) {
-                    CharSequence numbers = input.getText().toString();
-                    input.setText(numbers.subSequence(0, numbers.length() - 1));
-                }
-            }
-        });
-    };
-}
+        clear.setOnClickListener(view1 -> { //delete numbers one by one, from right to left
+            if (input.getText().length()>0) {
+                CharSequence numbers =input.getText().toString();
+                input.setText(numbers.subSequence(0, numbers.length()-1));
+        }
+    });
+}}
